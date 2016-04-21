d52e87adea893df7dcde4561451aff7357ffd736
Peach <568797992@qq.com>
2016-04-21 22:18:27 +0800

    memory

---
+++ app/src/main/java/com/cn/ac/ia/iot/dripirrigationsystem/adapter/NodeAdapter.java
@@ -0,0 +1,7 @@
+package com.cn.ac.ia.iot.dripirrigationsystem.adapter;
+
+/**
+ * Created by Peach on 2016/4/21.
+ */
+public class NodeAdapter {
+}
---
+++ app/src/main/java/com/cn/ac/ia/iot/dripirrigationsystem/obj/Node.java
@@ -0,0 +1,8 @@
+package com.cn.ac.ia.iot.dripirrigationsystem.obj;
+
+/**
+ * Created by Peach on 2016/4/21.
+ */
+public class Node {
+
+}
